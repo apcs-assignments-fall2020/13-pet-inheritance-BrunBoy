6adcb76dc9cfb9692e8cb5de2bf2b8452a7b9d8b
BrunBoy <23rreddy@athenian.org>
2020-11-02 02:15:02 +0000

    ithinkimDone

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,33 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
+    private int age;
+    private String name;
 
     // Constructors
-    
+    public Dog(String name, int age, String breed){
+        super(name, age);
+        this.breed=breed;
+    }
 
     // makeNoise() method
-
+    public Dog(){
+        System.out.println("bark");
+    }
 
     // toString method
-
+    public String toString(){
+        String str = "Name: " + name + ", Age: " + age + ", Breed: " + breed;
+        return str;
+    }
 
     // Getter
-
+    public String getBreed(){
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed){
+        this.breed=breed;
+    }
 }
